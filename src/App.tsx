ab281b4690334bff259eae87ba2ccff84908c51b
Lisa Meyer <lisamermey@gmail.com>
2022-09-26 16:21:46 -0400

    Set width and import Contact

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import CTA from './components/cta';
+import Contact from './components/contact';
 import './App.css';
 
 function App() {
   return (
-    <div className="App bg-neutral-200 h-screen flex justify-center items-center">
+    <div className="App bg-neutral-200 w-screen h-screen flex justify-center items-center">
       <CTA />
+      <Contact />
     </div>
   );
 }
