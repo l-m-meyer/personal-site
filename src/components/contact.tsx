57cbe58633a4d0cea36051cb194f8d5aebdb7346
Lisa Meyer <lisamermey@gmail.com>
2022-09-26 16:22:39 -0400

    Add contact component

---
+++ src/components/contact.tsx
@@ -0,0 +1,5 @@
+export default function Contact(){
+  return (
+    <h5 className="mr-2 self-center fixed right-0" style={{ writingMode: "vertical-rl" }}>lisamermey@gmail.com | 647.973.5758</h5>
+  );
+};
