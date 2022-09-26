f6e8f9b93efba5a35ad60b70e8611f4bcc2b07b2
Lisa Meyer <lisamermey@gmail.com>
2022-09-26 16:27:16 -0400

    Add position relative

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App bg-neutral-200 w-screen h-screen flex justify-center items-center">
+    <div className="App bg-neutral-200 w-screen h-screen flex justify-center items-center relative">
       <CTA />
       <Contact />
     </div>
