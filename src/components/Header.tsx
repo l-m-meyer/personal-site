60127d2f4bc50a129db5b6e6567c6683f1abf31d
Lisa Meyer <lisamermey@gmail.com>
2022-09-27 16:10:03 -0400

    Refactor Props interface

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,21 @@
 import profile from './lisa_profile.jpg';
 
-function Avatar() {
+interface Props {
+  name: string;
+  title?: string;
+  stack?: string;
+}
+
+function Avatar({name}: Props) {
   return (
     <img src={profile} alt="Lisa Meyer" className="h-40 rounded-full" />
   );
 }
 
-interface HeaderProps {
-  name: string;
-  title: string;
-  stack: string;
-}
-
-export default function Header({name, title, stack}: HeaderProps) {
+export default function Header({name, title, stack}: Props) {
   return (
     <>
-      <Avatar />
+      <Avatar name={name} />
       <div className="flex-col p-5 text-emerald-900 dark:text-neutral-200">
         <h1
           className="text-5xl font-bold "
