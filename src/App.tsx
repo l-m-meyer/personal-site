e861fdc9862d4b55c83aec8816742724c7a822c5
Lisa Meyer <lisamermey@gmail.com>
2022-09-27 13:15:10 -0400

    Add light/dark mode button and logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import { useState } from 'react';
 import Header from './components/Header';
 import Contact from './components/Contact';
 import './App.css';
 
 function App() {
+  const [darkToggle, setDarkToggle] = useState(false);
+
   return (
-    <div className="App bg-neutral-200 dark:bg-emerald-900 w-screen h-screen flex justify-center items-center relative">
-      <Header />
-      <Contact />
+    <div className={`App relative ${darkToggle ? 'dark' : ''}`}>
+      <button
+        onClick={() => setDarkToggle(!darkToggle)}
+        className="fixed top-5 right-5"
+        style={{ zIndex: 1 }}
+      >
+        Switch to Light | Dark
+      </button>
+      <div className='bg-neutral-200 dark:bg-emerald-900 w-screen h-screen flex justify-center items-center relative'>
+        <Header />
+        <Contact />
+      </div>
     </div>
   );
 }
