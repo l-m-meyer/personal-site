b2dc3584c20916367aff58a30ccb043af3a94fa3
Lisa Meyer <lisamermey@gmail.com>
2022-09-27 13:37:58 -0400

    Import icons and style light/dark button

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import Header from './components/Header';
 import Contact from './components/Contact';
 import './App.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro';
+
 function App() {
   const [darkToggle, setDarkToggle] = useState(false);
 
@@ -10,10 +13,17 @@ function App() {
     <div className={`App relative ${darkToggle ? 'dark' : ''}`}>
       <button
         onClick={() => setDarkToggle(!darkToggle)}
-        className="fixed top-5 right-5"
+        className="fixed top-5 right-5 bg-emerald-500 rounded-full p-2 w-20 flex justify-around"
         style={{ zIndex: 1 }}
       >
-        Switch to Light | Dark
+        <FontAwesomeIcon
+          icon={solid('moon')}
+          className={`${darkToggle ? 'text-emerald-100' : ''}`}
+        />
+        <FontAwesomeIcon
+          icon={solid('sun')}
+          className={`${darkToggle ? '' : 'text-emerald-100'}`}
+        />
       </button>
       <div className='bg-neutral-200 dark:bg-emerald-900 w-screen h-screen flex justify-center items-center relative'>
         <Header />
