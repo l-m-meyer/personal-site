0a4dfe6c11450dbb59378b8d0985402c8f1697e9
Lisa Meyer <lisamermey@gmail.com>
2022-09-28 14:21:01 -0400

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import ThemeToggle from './components/ThemeToggle';
 import Nav from './components/Nav';
