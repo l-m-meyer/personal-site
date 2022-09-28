d33bafc23462a04cf10d92ca790d2008cff20ca9
Lisa Meyer <lisamermey@gmail.com>
2022-09-28 10:52:50 -0400

    Create Home component

---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+
+export default function Home() {
+  return (
+    <Header
+      name="Lisa Meyer"
+      title="Full Stack Web Developer"
+      email="lisamermey@gmail.com"
+    />
+  );
+}
