5a1e581ca622ef104eedaf7c5ed87a25450294ac
Lisa Meyer <lisamermey@gmail.com>
2022-09-28 12:04:51 -0400

    Create routes and make semantic changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import ThemeToggle from './components/ThemeToggle';
 import Nav from './components/Nav';
+import About from './components/About/About';
+import Projects from './components/Projects/Projects';
+import Blog from './components/Blog/Blog';
 import './App.css';
 
 
@@ -10,19 +13,21 @@ function App() {
   const [darkToggle, setDarkToggle] = useState(false);
 
   return (
-    <div className={`App relative ${darkToggle ? 'dark' : ''}`}>
+    <main className={`App relative ${darkToggle ? 'dark' : ''}`}>
       <ThemeToggle
         darkToggle={darkToggle}
         setDarkToggle={setDarkToggle}
       />
       <Nav />
-      <main className='bg-neutral-200 dark:bg-emerald-900 w-screen h-screen flex justify-center items-center relative'>
+      <section className='bg-neutral-200 dark:bg-emerald-900 w-screen h-screen flex justify-center items-center relative'>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="about" element={<About />} /> */}
+          <Route path="about" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="blog" element={<Blog />} />
         </Routes>
-      </main>
-    </div>
+      </section>
+    </main>
   );
 }
 
