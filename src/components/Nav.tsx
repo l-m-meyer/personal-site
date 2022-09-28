3bc39a2fdfb494ec7bc1aa3bb47b757e6447b3ed
Lisa Meyer <lisamermey@gmail.com>
2022-09-28 12:03:45 -0400

    Add nav links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,8 +5,10 @@ export default function Nav() {
     <nav
       className='fixed top-5 left-10 flex space-x-10 text-emerald-900 dark:text-neutral-200'
       style={{ zIndex: 1 }}>
-      <Link to='/'>About</Link>
-      <Link to='/'>Projects</Link>
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/projects'>Projects</Link>
+      <Link to='/blog'>Blog</Link>
     </nav>
   );
 }
